050aed49b6caa5c1b9fcb73217c8253605e3c005
f3mo <fifemson@gmail.com>
2021-04-23 12:12:20 +0300

    Fixed IsADirectory error

--- duprm.py
+++ duprm.py
@@ -9,16 +9,24 @@ def file_tb():
         exit()
     else:
         for file in files:
-            with open(argv[1]+file, 'rb')as f:
-                file_dict[argv[1] +file] = hashlib.md5(f.read()).hexdigest()
+            if os.path.isfile(os.path.join(argv[1], file)):
+                with open(argv[1]+file, 'rb')as f:
+                    file_dict[argv[1] +file] = hashlib.md5(f.read()).hexdigest()
         return file_dict
 
-file_dict = file_tb()
-n_dict = {}
-for i , o in zip(file_dict.keys(), file_dict.values()):
-    if i  not in n_dict.keys() and o not in n_dict.values():
-        n_dict[i] = o
-for i in file_dict.keys():
-    if i not in n_dict.keys():
-        print(f'File { i.split("/")[-1] } has been deleted')
-        os.remove(i)
+
+def main():
+    file_dict = file_tb()
+    n_dict = {}
+    for i , o in zip(file_dict.keys(), file_dict.values()):
+        if i  not in n_dict.keys() and o not in n_dict.values():
+            n_dict[i] = o
+    for i in file_dict.keys():
+        if i not in n_dict.keys():
+            print(f'File { i.split("/")[-1] } has been deleted')
+            os.remove(i)
+
+
+
+if __name__ == '__main__':
+    main()
