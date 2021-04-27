f9ecf5e93dcb03bab21c12575b117a1ee7bb1752
f3mo <fifemson@gmail.com>
2021-04-27 12:23:11 +0300

    made it readable

--- duprm.py
+++ duprm.py
@@ -1,32 +1,45 @@
-import hashlib
-import os
-from sys import argv
+from hashlib import md5
+from os import path, listdir, remove
+from base64 import b64encode
+from sys import argv, exit
 
-def file_tb():
-    file_dict = {}
-    files = os.listdir(argv[1])
-    if len(argv) < 2:
-        exit()
-    else:
-        for file in files:
-            if os.path.isfile(os.path.join(argv[1], file)):
-                with open(argv[1]+file, 'rb')as f:
-                    file_dict[argv[1] +file] = hashlib.md5(f.read()).hexdigest()
-        return file_dict
+def get_file_list():
+	file_list = []
+	if len(argv) < 2 or path.isdir(argv[1]) != True:
+		print('Choose a directory')
+		exit(5)	
+	else:
+		for file_ in listdir(argv[1]):
+			if path.isfile(path.join(argv[1], file_)) == True:	
+				file_list.append(path.join(argv[1], file_))
+		return file_list 
 
+
+def hash_file_list():
+	file_list = get_file_list()
+	file_hash_map = {}
+	file_hash = ''
+	for file_ in file_list:
+		with open(file_, 'rb') as f:
+			file_hash = md5(f.read()).digest()
+			file_hash = b64encode(file_hash).decode()
+			file_hash_map[file_] = file_hash
+	return file_hash_map
 
 def main():
-    file_dict = file_tb()
-    n_dict = {}
-    for i , o in zip(file_dict.keys(), file_dict.values()):
-        if i  not in n_dict.keys() and o not in n_dict.values():
-            n_dict[i] = o
-    for i in file_dict.keys():
-        if i not in n_dict.keys():
-            print(f'File { i.split("/")[-1] } has been deleted')
-            os.remove(i)
-
-
+	file_hash_map = hash_file_list()
+	sorted_files = {}
+	duplicates = []
+	for key, value  in zip(file_hash_map, file_hash_map.values()):
+		if key not in sorted_files and value not in sorted_files.values():
+			sorted_files[key] = value
+		if key not in sorted_files:
+			duplicates.append(key)
+	print(f' {len(duplicates)} duplicates Found')
+	for key in file_hash_map: 
+		if key not in  sorted_files:
+			remove(key)
+			print(f' File {key} was deleted')
 
 if __name__ == '__main__':
     main()
